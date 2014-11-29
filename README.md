22f15ef95253ba28a5327071589a142cbb1f5677
Vincenzo Pii <vinc.pii@gmail.com>
2014-11-29 16:15:30 +0100

    Example for simple search

--- README.md
+++ README.md
@@ -3,6 +3,18 @@ uguitar-python
 
 python bindings for ultimate-guitar
 
+Example
+-------
+
+    import uguitar.ug_simplesearch
+    
+    tabs = uguitar.ug_simplesearch.search("metallica one")
+            for tab in tabs:
+                print tab._uri
+                print tab._rating
+                print tab._ratingCounter
+                print tab._type
+
 Requirements
 ------------
 
--- test/content.py
+++ test/content.py
@@ -5,6 +5,7 @@ import unittest
 from uguitar.http_curl import HttpCurl
 from uguitar.ug_request import UgRequest
 from uguitar.ug_parser import UgParser
+import uguitar.ug_simplesearch
 
 class ContentTest(unittest.TestCase):
 
@@ -19,6 +20,15 @@ class ContentTest(unittest.TestCase):
         # The first page of this request should return 53 results.
         # If they clean up tabs and remove some, this will fail!
         assert(len(parser.getTabs()) is 53)
+
+    def testSimpleSearch(self):
+        tabs = uguitar.ug_simplesearch.search("metallica one")
+        for tab in tabs:
+            print tab._uri
+            print tab._rating
+            print tab._ratingCounter
+            print tab._type
+
 
 if __name__ == '__main__':
     unittest.main()
